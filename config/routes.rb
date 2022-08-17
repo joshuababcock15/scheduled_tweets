fcbe944757eaefb5dcacf07ea24d59e02a6ef26c
joshuababcock15 <Joshuababcock15@gmail.com>
2022-08-17 08:17:11 -0400

    Setting up links and bootstrap css

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "about", to: "about#index"
+  get "about-us", to: "about#index", as: :about
 
-  # Defines the root path route ("/")
+ root to: "main#index"
   # root "articles#index"
 end
