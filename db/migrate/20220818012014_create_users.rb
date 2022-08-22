1f52b9830db2c929b463482e0b432e520989ce75
joshuababcock15 <Joshuababcock15@gmail.com>
2022-08-22 15:31:46 -0400

    Vide0 12: Adding email validation

--- db/migrate/20220818012014_create_users.rb
+++ db/migrate/20220818012014_create_users.rb
@@ -1,7 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :email
+      # adding the null is false then you have to put in a email
+      t.string :email, null: false
       t.string :password_digest
 
       t.timestamps
